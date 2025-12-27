84ec0818085419a867724aee9fc94a8ac5a72c46
Nate Douglas <ndouglas@users.noreply.github.com>
2025-12-26 20:56:11 -0500

    Fix crlf test.

--- test/clork/utils_test.clj
+++ test/clork/utils_test.clj
@@ -1,5 +1,7 @@
 (in-ns 'clork.core-test)
 
 (deftest crlf-test
-  (testing "(crlf) prints a newline"
-    (is (= "\n" (with-out-str (crlf))))))
+  (testing "(crlf game-state) prints a newline and returns game-state"
+    (let [game-state {:test true}]
+      (is (= "\n" (with-out-str (crlf game-state))))
+      (is (= game-state (crlf game-state))))))
